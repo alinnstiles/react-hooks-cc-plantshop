6ccc95d199b6959edace54a731da5c2580cd29fe
Alex Stiles <alinnstiles@gmail.com>
2024-04-16 16:19:10 -0400

    lab complete

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,16 +1,16 @@
-import React from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+import React from "react";
 
-function PlantPage() {
+function PlantPage({handleDelete}) {
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+
+      <PlantList  handleDelete={handleDelete}/>
     </main>
   );
 }
-
 export default PlantPage;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PlantContext } from "./App";
 
 function Search() {
+  const { search, handleSearchChange } = useContext(PlantContext);
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
+        value={search}
         type="text"
         id="search"
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
-      />
-    </div>
-  );
-}
 
-export default Search;
+        onChange={handleSearchChange}
+        />
+      </div>
+    );
+  }
+  
+  export default Search;
